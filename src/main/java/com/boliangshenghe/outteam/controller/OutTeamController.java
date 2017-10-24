b27e8b44ea31f40b572d7a5f8d9b250f36db0c51
superxuzj <271652260@qq.com>
2017-10-24 15:25:21 +0800

    add

--- src/main/java/com/boliangshenghe/outteam/controller/OutTeamController.java
+++ src/main/java/com/boliangshenghe/outteam/controller/OutTeamController.java
@@ -6,17 +6,14 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.boliangshenghe.outteam.common.PageBean;
 import com.boliangshenghe.outteam.entity.Outteam;
@@ -28,7 +25,6 @@ import com.boliangshenghe.outteam.service.OutteamDetailService;
 import com.boliangshenghe.outteam.service.OutteamService;
 import com.boliangshenghe.outteam.service.UserService;
 import com.boliangshenghe.outteam.util.FlightUtils;
-import com.boliangshenghe.outteam.util.JsonUtils;
 
 /**
  * 出队管理
