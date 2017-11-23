295654336d61f3aedf63c06b7c753b7fcebe3df5
superxuzj <271652260@qq.com>
2017-11-23 17:33:04 +0800

    11.23

---
+++ src/main/java/com/boliangshenghe/outteam/activemq/TopicReceiver.java
@@ -0,0 +1,23 @@
+package com.boliangshenghe.outteam.activemq;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicReceiver implements MessageListener {
+
+	public void onMessage(Message message) {
+		try {
+			System.out.println("maven  ---TopicReceiver1接收到消息:"
+					+ ((TextMessage) message).getText());
+			
+			//Result result = JsonUtils.toBean(output, Result.class);//result对象
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
